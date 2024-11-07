2213fb1b911b8d0e77fb6c513aeaa8a50b313106
JeHeeYu <sees1234558@gmail.com>
2024-11-07 14:20:55 +0900

    build: Add realsense2 envrionment configuration

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <librealsense2/rs.hpp>
+
+int main() {
+    try {
+        rs2::context context;
+        auto list = context.query_devices();
+
+        if (list.size() == 0) {
+            std::cout << "No RealSense devices connected." << std::endl;
+            return -1;
+        }
+
+        for (auto&& dev : list) {
+            std::cout << "Device detected: " << dev.get_info(RS2_CAMERA_INFO_NAME) << std::endl;
+        }
+    } catch (const rs2::error &e) {
+        std::cerr << "RealSense error calling " << e.get_failed_function()
+                  << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
